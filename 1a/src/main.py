e069b7077160fc9faf157375ad8a5c3d186d0a20
= <ceyhuncakir@live.nl>
2024-09-12 07:48:37 +0200

    added all working

--- 1a/src/main.py
+++ 1a/src/main.py
@@ -5,18 +5,18 @@ from ml.mlp import MultiLayerPerceptron
 
 if __name__ == "__main__":
 
-    # Baseline_1(
-    #     dataset_dir_path="/home/x7a/Documents/Github/INFOMAIR/1a/data/dialog_acts.dat"
-    # ).run()
+    Baseline_1(
+        dataset_dir_path="/home/x7a/Documents/Github/INFOMAIR/1a/data/dialog_acts.dat"
+    ).run()
 
-    # Baseline_2(
-    #     dataset_dir_path="/home/x7a/Documents/Github/INFOMAIR/1a/data/dialog_acts.dat"
-    # ).run()
+    Baseline_2(
+        dataset_dir_path="/home/x7a/Documents/Github/INFOMAIR/1a/data/dialog_acts.dat"
+    ).run()
 
-    # Xgboost(
-    #     dataset_dir_path="/home/x7a/Documents/Github/INFOMAIR/1a/data/dialog_acts.dat",
-    #     doc2vec_data_dir_path="/home/x7a/Documents/Github/INFOMAIR/1a/data/gensim/doc2vec.model"
-    # ).run()
+    Xgboost(
+        dataset_dir_path="/home/x7a/Documents/Github/INFOMAIR/1a/data/dialog_acts.dat",
+        doc2vec_data_dir_path="/home/x7a/Documents/Github/INFOMAIR/1a/data/gensim/doc2vec.model"
+    ).run()
 
     MultiLayerPerceptron(
         dataset_dir_path="/home/x7a/Documents/Github/INFOMAIR/1a/data/dialog_acts.dat",
