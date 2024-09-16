9f2a27efae1f38eb8896b6d889c4cb3a834cc5cd
= <ceyhuncakir@live.nl>
2024-09-16 03:16:38 +0200

    added some fixes to xgboost

--- 1a/src/ml/xgboost.py
+++ 1a/src/ml/xgboost.py
@@ -16,6 +16,17 @@ from helpers.evaluation import Evaluate
 xgboost_app = typer.Typer()
 
 class Xgboost(Base):
+    """
+    This class houses the training, evaluation and usage of the xgboost model.
+
+    Attributes:
+        dataset_dir_path (str): The dataset directory path where the original dialog acts dataset resides in.
+        doc2vec_data_dir_path (str): The dataset directory path where the trained doc2vec model resides in.
+        checkpoint_dir_path (str): The checkpoint directory path where the trainable xgboost model will be saved in.
+        experiment_name (str): The experiment name of the xgboost model that will be saved as.
+        deduplication (bool): Whether the xgboost model should be trained on deduplicated data from dialog acts dataset.
+    """
+
     def __init__(
         self,
         dataset_dir_path: str,
@@ -51,6 +62,18 @@ class Xgboost(Base):
         verbosity: int,
         num_round: int,
     ) -> None:
+        """
+        This function is being used as the training function for the xgboost model.
+
+        Args:
+            train (pd.DataFrame): A dataframe consisting of the training examples for the xgboost model.
+            validation (pd.DataFrame): A dataframe consisting of the validation samples for the xgboost model.
+            verbosity (int): The verbosity level thats set for the training phase.
+            num_round (int): The number of boosting rounds for the xgboost tree.
+
+        Returns:
+            model: The trained xgboost model.
+        """
 
         params = {
             'device': 'cuda',
@@ -82,6 +105,15 @@ class Xgboost(Base):
         self,
         model
     ) -> None:
+        """
+        This function is neded to save the trained xgboost model.
+
+        Args:
+            model: The trained xgboost model.
+
+        Returns:
+            None
+        """
 
         os.makedirs(f"{self._checkpoint_dir_path}/{self._experiment_name}", exist_ok=True)
         
@@ -91,6 +123,15 @@ class Xgboost(Base):
     def _load_model(
         self
     ) -> None:
+        """
+        This function is needed to load in the xgboost model.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
         
         xgb_classifier = xgb.Booster()
         xgb_classifier.load_model(f'{self._checkpoint_dir_path}/{self._experiment_name}/xgboost.json')
@@ -101,9 +142,20 @@ class Xgboost(Base):
     def _evaluate_test(
         self,
         model,
-        test,
-        labels
+        test: pd.DataFrame,
+        labels: List[str]
     ) -> None:
+        """
+        This function is needed to evaluate / predict the given test set.
+
+        Args:
+            model: The trained xgboost model we are using for prediction.
+            test (pd.DataFrame): The test set that needs to be evaluated / predicted.
+            labels (List[str]): A list containing the labels which will translate the predicted values into categorical values. 
+
+        Returns:
+            pd.DataFrame: The test set that has been evaluated based on the trained xgboost model.
+        """
         
         ndarray_embeddings_test = np.vstack(test['embeddings'].values)
 
@@ -121,6 +173,15 @@ class Xgboost(Base):
     def _load_doc2vec(
         self
     ) -> None:
+        """
+        This function loads in the trained doc2vec model based on the given data directory.
+
+        Args:
+            None
+
+        Returns:
+            model: The doc2vec model that has been loaded in.
+        """
         
         return Doc2Vec.load(self._doc2vec_data_dir_path)
     
@@ -129,12 +190,22 @@ class Xgboost(Base):
         self,
         data: list[pd.DataFrame, ...],
         model
-    ) -> None:
+    ) -> list[pd.DataFrame, ...]:
+        """
+        This function is needed to generate the necessary embeddings based on the trained doc2vec model.
+
+        Args:
+            data (list[pd.DataFrame, ...]): A list containing the individual sets where training, validation, and testing will happen on.
+            model (): The doc2vec model which is loaded in to generate embeddings for every utterance occurence.
+
+        Returns:
+            list[pd.DataFrame, ...]: A list containing the individual sets where training, validation, and testing will happen on, with the embeddings,
+        """
         
         for datasets in data:
             
             datasets['embeddings'] = datasets['utterance'].apply(
-                lambda x: np.array(model.infer_vector(x.split()))
+                lambda utterance: np.array(model.infer_vector(utterance.split()))
             )
 
         return data
@@ -145,6 +216,17 @@ class Xgboost(Base):
         df: pd.DataFrame,
         labels: List[str] 
     ) -> Tuple[pd.DataFrame, pd.DataFrame]:
+
+        """
+        This function is needed to split the initial splitted dataset into a train and validation set.
+
+        Args:
+            df (pd.DataFrame): A pandas dataframe including the initial train set.
+            labels (List[str]): A list including all the labels we need to determine the target.
+
+        Returns:
+            Tuple[pd.DataFrame, pd.DataFrame]: A tuple consisting of two pandas dataframes both the training and validation set for the xgboost training.
+        """
 
         train, validation = train_test_split(df, train_size=0.80, random_state=42)
 
@@ -158,10 +240,18 @@ class Xgboost(Base):
         self,
         utterance: str
     ) -> str:
+        """
+        This Function is used to use the trained xgboost model inference
+
+        Args:
+            utterance (str): The utterance thats derived from the users inputs.
+
+        Returns:
+            str: A categorical predicted value.
+        """
 
         xgb_classfier = self._load_model()
 
-        # preprocess
         utterance = utterance.lower().lstrip()
 
         embeddings = np.array(self.doc2vec.infer_vector(utterance.split())).reshape(1, -1)
@@ -169,6 +259,7 @@ class Xgboost(Base):
         dtest = xgb.DMatrix(embeddings)
 
         y_preds_proba = xgb_classfier.predict(dtest)
+
         index_array = np.argmax(y_preds_proba, axis=-1)
 
         categorical_pred = self.labels[index_array[0]]
@@ -181,6 +272,15 @@ class Xgboost(Base):
     def evaluate(
         self
     ) -> None:
+        """
+        This function is needed to evaluate the xgboost classifier we have trained.
+
+        Args:
+            None
+        
+        Returns:
+            None
+        """
 
         xgb_classfier = self._load_model()
         
@@ -202,6 +302,16 @@ class Xgboost(Base):
         verbosity: int,
         num_round: int
     ) -> None:
+        """
+        This function is meant to be the main train function used for the xgboost classfiier.
+
+        Args:
+            verbosity (int): The verbosity level defined inside the training function of the xgboost classfier.
+            num_round (int): The number of rounds we need to boost the classifier for.
+
+        Returns:
+            None
+        """
 
         xgb_classfier = self._train_model(
             train=self.model_train,
@@ -214,13 +324,13 @@ class Xgboost(Base):
 
 @xgboost_app.command()
 def evaluate(
-    dataset_dir_path: Annotated[str, typer.Option(help="The dataset dir path we want to specify for the dataset.")] = None,
-    doc2vec_data_dir_path: Annotated[str, typer.Option(help="The dataset dir path we want to specify for the dataset.")] = None,
-    checkpoint_dir_path: Annotated[str, typer.Option(help="The dataset dir path we want to specify for the dataset.")] = None,
-    experiment_name: Annotated[str, typer.Option(help="The dataset dir path we want to specify for the dataset.")] = None,
-    deduplication: Annotated[bool, typer.Option(help="The dataset dir path we want to specify for the dataset.")] = None,
+    dataset_dir_path: Annotated[str, typer.Option(help="The dataset directory path where the original dialog acts dataset resides in.")] = None,
+    doc2vec_data_dir_path: Annotated[str, typer.Option(help="The dataset directory path where the trained doc2vec model resides in.")] = None,
+    checkpoint_dir_path: Annotated[str, typer.Option(help="The checkpoint directory path where the trainable xgboost model will be saved in.")] = None,
+    experiment_name: Annotated[str, typer.Option(help="The experiment name of the xgboost model that will be saved as.")] = None,
+    deduplication: Annotated[bool, typer.Option(help="Whether the xgboost model should be trained on deduplicated data from dialog acts dataset.")] = None,
 ) -> None:
-    
+
     Xgboost(
         dataset_dir_path=dataset_dir_path,
         doc2vec_data_dir_path=doc2vec_data_dir_path,
@@ -231,11 +341,11 @@ def evaluate(
 
 @xgboost_app.command()
 def inference(
-    dataset_dir_path: Annotated[str, typer.Option(help="The dataset dir path we want to specify for the dataset.")] = None,
-    doc2vec_data_dir_path: Annotated[str, typer.Option(help="The dataset dir path we want to specify for the dataset.")] = None,
-    checkpoint_dir_path: Annotated[str, typer.Option(help="The dataset dir path we want to specify for the dataset.")] = None,
-    experiment_name: Annotated[str, typer.Option(help="The dataset dir path we want to specify for the dataset.")] = None,
-    deduplication: Annotated[bool, typer.Option(help="The dataset dir path we want to specify for the dataset.")] = None,
+    dataset_dir_path: Annotated[str, typer.Option(help="The dataset directory path where the original dialog acts dataset resides in.")] = None,
+    doc2vec_data_dir_path: Annotated[str, typer.Option(help="The dataset directory path where the trained doc2vec model resides in.")] = None,
+    checkpoint_dir_path: Annotated[str, typer.Option(help="The checkpoint directory path where the trainable xgboost model will be saved in.")] = None,
+    experiment_name: Annotated[str, typer.Option(help="The experiment name of the xgboost model that will be saved as.")] = None,
+    deduplication: Annotated[bool, typer.Option(help="Whether the xgboost model should be trained on deduplicated data from dialog acts dataset.")] = None,
 ) -> None:
     
     xgboost = Xgboost(
@@ -254,13 +364,13 @@ def inference(
 
 @xgboost_app.command()
 def train(
-    dataset_dir_path: Annotated[str, typer.Option(help="The dataset dir path we want to specify for the dataset.")] = None,
-    doc2vec_data_dir_path: Annotated[str, typer.Option(help="The dataset dir path we want to specify for the dataset.")] = None,
-    checkpoint_dir_path: Annotated[str, typer.Option(help="The dataset dir path we want to specify for the dataset.")] = None,
-    experiment_name: Annotated[str, typer.Option(help="The dataset dir path we want to specify for the dataset.")] = None,
-    verbosity: Annotated[int, typer.Option(help="The dataset dir path we want to specify for the dataset.")] = None,
-    num_rounds: Annotated[int, typer.Option(help="The dataset dir path we want to specify for the dataset.")] = None,
-    deduplication: Annotated[bool, typer.Option(help="The dataset dir path we want to specify for the dataset.")] = None,
+    dataset_dir_path: Annotated[str, typer.Option(help="The dataset directory path where the original dialog acts dataset resides in.")] = None,
+    doc2vec_data_dir_path: Annotated[str, typer.Option(help="The dataset directory path where the trained doc2vec model resides in.")] = None,
+    checkpoint_dir_path: Annotated[str, typer.Option(help="The checkpoint directory path where the trainable xgboost model will be saved in.")] = None,
+    experiment_name: Annotated[str, typer.Option(help="The experiment name of the xgboost model that will be saved as.")] = None,
+    verbosity: Annotated[int, typer.Option(help="The verbosity level is being set inside the model training phase.")] = None,
+    num_rounds: Annotated[int, typer.Option(help="The number of boosting rounds inside the model training phase.")] = None,
+    deduplication: Annotated[bool, typer.Option(help="Whether the xgboost model should be trained on deduplicated data from dialog acts dataset.")] = None,
 ) -> None:
     
     Xgboost(
