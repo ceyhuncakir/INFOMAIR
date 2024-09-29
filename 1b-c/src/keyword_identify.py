d40378831942f784bd10c95355713e923212f977
pingpingi <hueluxx@gmail.com>
2024-09-29 20:20:13 +0200

    combine 1b and 1c

---
+++ 1b-c/src/keyword_identify.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from Levenshtein import distance
+
+def identify_keywords(sentence, state):
+    df = pd.read_csv('1b-c/data/restaurant_info_extra.csv')
+
+    common_words = ['I', 'the', 'an', 'in', 'is', 'for', 'can', 'have', 'am', 'need', 'want', 'what', 'that', 'this', 'their', 'its', 'yes', 'no', 'eat', 'else']
+    any_conditions = {
+        'food': ['any food'],
+        'area': ['anywhere', 'any place', 'any area'],
+        'pricerange': ['any price', 'any pricerange'],
+        'general': ['any', 'anything', 'dont care', 'doesnt matter', 'you choose']
+    }
+
+    keyword_dict = {
+    'pricerange': df['pricerange'].dropna().astype(str).unique(), 
+    'area': df['area'].dropna().astype(str).unique(),
+    'food': df['food'].dropna().astype(str).unique()
+    }
+
+    words = sentence.lower().split()
+    keywords_found = {}
+    min_distance = {} 
+
+    for attribute, phrases in any_conditions.items(): # hanle more specific 'dont care' values that can be given anytime. E.g., "anywhere"
+        if attribute == 'general':
+            continue
+        for phrase in phrases:
+            if phrase in sentence.lower():
+                keywords_found[attribute] = 'any'
+                break
+
+    for phrase in any_conditions['general']: # handle 'dontcare' value per state. E.g., if user input includes 'any' when asked for food in state 3. Assume user is referring to food.
+        if phrase in sentence.lower():
+            if state == 2:
+                return {'area': 'any'}
+            elif state == 3:
+                return {'food': 'any'}
+            elif state == 4:
+                return {'pricerange': 'any'}
+            break
+
+    for n in range(1, 3): # scan for keyword phrases consisting of one or two words. Some keyword phrases consists of two words, such as 'asian oriental'
+        for i in range(len(words) - n + 1):
+            phrase = ' '.join(words[i:i + n])
+            for key, keywords in keyword_dict.items():
+                if phrase in common_words: # skip common words to prevent confusion
+                    continue
+                elif phrase in keywords: # if phrase is directly found in the keywords, return that keyword
+                    keywords_found[key] = phrase
+                    min_distance[key] = 0
+                else:
+                    for keyword in keywords: 
+                        phrase_dist = distance(phrase, keyword) # if not found diretly, calculate Levenshtein distance for each phrase and each keyword
+                        if phrase_dist < 3:
+                            if key not in min_distance or phrase_dist < min_distance[key]: # keep only smallest distance and distances below 3
+                                min_distance[key] = phrase_dist
+                                keywords_found[key] = keyword
+
+    return keywords_found
---
+++ 1b-c/src/lookup.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+def lookup_restaurant(pricerange=None, area=None, food=None, foodquality=None, crowdedness=None, lengthofstay=None, exclusion_list=[]):
+    
+    def filter_any(value):
+        return None if value is not None and value == 'any' else value #if value is 'any', change this back to None so that all results are returned
+
+    pricerange = filter_any(pricerange)
+    area = filter_any(area)
+    food = filter_any(food)
+    foodquality = filter_any(foodquality)
+    crowdedness = filter_any(crowdedness)
+    lengthofstay = filter_any(lengthofstay)
+
+    df = pd.read_csv('1b-c/data/restaurant_info_extra.csv')
+    results = df
+
+    if pricerange:
+        results = results[results['pricerange'] == pricerange.lower()]
+    if area:
+        results = results[results['area'] == area.lower()]
+    if food:
+        results = results[results['food'] == food.lower()]
+    if foodquality:
+        results = results[results['foodquality'] == foodquality.lower()]
+    if crowdedness:
+        results = results[results['crowdedness'] == crowdedness.lower()]
+    if lengthofstay:
+        results = results[results['lengthofstay'] == lengthofstay.lower()]
+    
+    if exclusion_list:
+        results = results[~results['food'].isin([x.lower() for x in exclusion_list])] # remove food types that are in the exclusion list from the results.
+
+    return results
